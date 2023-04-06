80485540f046b77684488c50a5ee90b1c984708b
dmitry-salnikov-msu <75671211+dmitry-salnikov-msu@users.noreply.github.com>
2023-04-06 13:52:09 +0300

    Update solution.py

--- solution.py
+++ solution.py
@@ -1,15 +1,18 @@
 import pandas as pd
 import numpy as np
 
-from scipy.stats import norm
+from scipy.stats import norm, expon
 
 
 chat_id = 626925789 # Ваш chat ID, не меняйте название переменной
 
 def solution(p: float, x: np.array) -> tuple:
-    x = 2*(x + 1/2)/95**2
+    #x = 2*(x + 1/2)/95**2
     alpha = 1 - p
     loc = x.mean()
-    scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
-    return loc - scale * norm.ppf(1 - alpha / 2), \
-           loc - scale * norm.ppf(alpha / 2)
+    #scale = np.sqrt(np.var(x)) / np.sqrt(len(x))
+    left   = 2*(loc - 1/2 + expon.ppf(alpha/2))/95**2
+    right  = 2*(loc - 1/2 + expon.ppf(1 - alpha/2))/95**2
+    #return loc - scale * norm.ppf(1 - alpha / 2), \
+    #       loc - scale * norm.ppf(alpha / 2)
+    return left,right
